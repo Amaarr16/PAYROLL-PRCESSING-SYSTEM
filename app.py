d65ff62d16ab803ca6a11110085eac57da9914d5
Ayush Goel <aayushgoel524@gmail.com>
2023-11-05 01:27:54 +0530

    UPDATE-2

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-#from flaskext.mysql import MySQL
-#from flask_mysqldb import MySQL
 import os
-import mysql
 import mysql.connector
 
 app = Flask(__name__)
@@ -14,9 +11,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Psatpsat'
 app.config['MYSQL_DB'] = 'payroll_processing'  # Use the name of your database
-# Remove this line, as you don't need it for 'mysql.connector'
-#mysql = MySQL(app)
-# Create a MySQL connection and cursor
+
 def get_mysql_connection():
     return mysql.connector.connect(
         host=app.config['MYSQL_HOST'],
@@ -24,7 +19,7 @@ def get_mysql_connection():
         password=app.config['MYSQL_PASSWORD'],
         database=app.config['MYSQL_DB']
     )
-# Function to create the database if it doesn't exist
+
 def create_database_if_not_exists():
     try:
         connection = mysql.connector.connect(
@@ -54,79 +49,82 @@ def create_employee_table_if_not_exists():
             emp_dob DATE,
             emp_mobile_no VARCHAR(15),
             emp_email VARCHAR(255),
-            emp_password VARCHAR(255)
+            emp_password VARCHAR(255),
+            employer VARCHAR(255),
+            employer_logo_url VARCHAR(255)
         )
         """)
-
         connection.commit()
         cursor.close()
         connection.close()
-
     except Exception as e:
         print(f"Error creating 'employee' table: {e}")
 
+create_employee_table_if_not_exists()
 
 @app.route('/')
 def index():
-    # Handle the index page
-    # You can fetch data from the database and pass it to the template
     return render_template('index.html')
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    # Handle the registration page
     if request.method == 'POST':
         emp_name = request.form['emp_name']
         emp_designation = request.form['emp_designation']
         emp_dob = request.form['emp_dob']
         emp_mobile_no = request.form['emp_mobile_no']
         emp_email = request.form['emp_email']
-        # Handle the form submission and insert data into the database
-        # You can use mysql to interact with the database
-        # Create a MySQL connection and cursor
+        emp_password = request.form['emp_password']
+        employer='Payroll Processing System'
+        employer_logo_url='PAYROLL-IMG.jpg'
         connection = get_mysql_connection()
         cursor = connection.cursor()
         cursor.execute(
-            "INSERT INTO employee (emp_name, emp_designation, emp_dob, emp_mobile_no, emp_email) VALUES (%s, %s, %s, %s, %s)",
-            (emp_name, emp_designation, emp_dob, emp_mobile_no, emp_email)
-        )
-        # Execute SQL queries to insert data
-        # Example: cursor.execute("INSERT INTO employee (emp_name, emp_designation, ...) VALUES (%s, %s, ...)", (form_data))
+            "INSERT INTO employee (emp_name, emp_designation, emp_dob, emp_mobile_no, emp_email, emp_password, employer, employer_logo_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+            (emp_name, emp_designation, emp_dob, emp_mobile_no, emp_email, emp_password, employer, employer_logo_url)
+            )
         connection.commit()
         cursor.close()
         connection.close()
         return redirect(url_for('index'))
-    return render_template('/registration.html')
+    return render_template('registration.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         emp_email = request.form['emp_email']
         emp_password = request.form['emp_password']
-
-        # Create a MySQL connection and cursor
         connection = get_mysql_connection()
-        cursor = connection.cursor(dictionary=True)  # Fetch results as dictionaries
+        cursor = connection.cursor(dictionary=True)
         cursor.execute("SELECT * FROM employee WHERE emp_email = %s", (emp_email,))
         user = cursor.fetchone()
-        
         if user and user['emp_password'] == emp_password:
-            # Authentication successful
-            # You can set the user's session here
             session['user_id'] = user['emp_id']
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid email or password. Please try again.', 'error')
-
     return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
-    # Check if the user is authenticated
     if 'user_id' in session:
-        return render_template('dashboard.html')
+        connection = get_mysql_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM employee WHERE emp_id = %s", (session['user_id'],))
+        employee = cursor.fetchone()
+        connection.close()
+        if employee:
+            employer_logo_url = employee.get('employer_logo_url', '')
+            return render_template('dashboard.html', employee=employee, employer_logo_url=employer_logo_url)
+        else:
+            return "Employee not found"
     else:
         return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)  # Remove the user_id from the session
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     create_employee_table_if_not_exists()
